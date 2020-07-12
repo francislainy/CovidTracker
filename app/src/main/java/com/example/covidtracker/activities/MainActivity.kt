0e3df08de38f6a483c5e4ab1cfd13b53332cb533
Francislainy Campos <francislainy.campos@hmhco.com>
2020-07-12 03:34:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/covidtracker/activities/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.covidtracker.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.covidtracker.R
+import com.example.covidtracker.activities.fragments.UpdatesFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val currentFragment =
+                supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if (currentFragment == null) {
+            val fragment = UpdatesFragment.newInstance()
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit()
+        }
+
+    }
+}
