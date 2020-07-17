328d4f8092894e621d92bf797bd6914756303896
Francislainy Campos <francislainy.campos@hmhco.com>
2020-07-17 17:50:38 +0100

    Add dynamic lists to be given to dialog depending on which option has been selected

--- app/src/main/java/com/example/covidtracker/fragments/CovidCheckInFragment.kt
+++ app/src/main/java/com/example/covidtracker/fragments/CovidCheckInFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.covidtracker.R
@@ -28,8 +29,19 @@ class CovidCheckInFragment : Fragment() {
 
         navController = Navigation.findNavController(view)
 
-        headerHolder.setOnClickListener {
-            navController!!.navigate(R.id.action_covidCheckInFragment_to_my_dialog_fragment)
+        etYourAge.setOnClickListener {
+            val bundle = bundleOf("headerText" to "Choose your age range")
+            navController!!.navigate(R.id.action_covidCheckInFragment_to_my_dialog_fragment, bundle)
+        }
+
+        etYourCounty.setOnClickListener {
+            val bundle = bundleOf("headerText" to "Choose your county")
+            navController!!.navigate(R.id.action_covidCheckInFragment_to_my_dialog_fragment, bundle)
+        }
+
+        etYourLocality.setOnClickListener {
+            val bundle = bundleOf("headerText" to "Choose your locality")
+            navController!!.navigate(R.id.action_covidCheckInFragment_to_my_dialog_fragment, bundle)
         }
 
     }
