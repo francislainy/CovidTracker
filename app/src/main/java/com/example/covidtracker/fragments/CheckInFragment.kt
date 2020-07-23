0cc440b78cd2f536fe6aa4f6d67c4be47554169a
Francislainy Campos <francislainy.campos@hmhco.com>
2020-07-23 05:01:32 +0100

    Fix null error when data posted when view resumed by having code within onAttach instead.

--- app/src/main/java/com/example/covidtracker/fragments/CheckInFragment.kt
+++ app/src/main/java/com/example/covidtracker/fragments/CheckInFragment.kt
@@ -1,5 +1,6 @@
 package com.example.covidtracker.fragments
 
+import android.app.Activity
 import android.os.Build
 import android.os.Bundle
 import android.view.View
@@ -8,12 +9,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.covidtracker.db.DataRoomDbase
 import com.example.covidtracker.R
-import com.example.covidtracker.utils.Utils
 import com.example.covidtracker.activities.MainActivity
 import com.example.covidtracker.adapter_holders.RecyclerHistoryItem
 import com.example.covidtracker.model.MyDataList
+import com.example.covidtracker.utils.Utils
 import com.example.covidtracker.view_models.PostViewModel
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
@@ -21,10 +21,10 @@ import kotlinx.android.synthetic.main.fragment_check_in_bottom.*
 import kotlinx.android.synthetic.main.title_and_progress_bar.*
 import java.text.SimpleDateFormat
 
+
 class CheckInFragment : Fragment(R.layout.fragment_check_in_bottom) {
 
     private var adapter: GroupAdapter<GroupieViewHolder>? = null
-    private var myDatabase: DataRoomDbase? = null
 
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -39,15 +39,16 @@ class CheckInFragment : Fragment(R.layout.fragment_check_in_bottom) {
         adapter = GroupAdapter()
         rvHistory.adapter = adapter
 
+    }
+
+    override fun onAttach(activity: Activity) {
+        super.onAttach(activity)
 
         val postViewModel = ViewModelProviders.of(this).get(PostViewModel::class.java)
         postViewModel.allPosts.observe(activity as MainActivity,
             Observer { posts: List<MyDataList?>? ->
 
                 for (p in posts!!) {
-
-                    myDatabase = DataRoomDbase.getDatabase(activity as MainActivity)
-                    val list = myDatabase?.dataDAO()?.myData
 
                     val date = SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy").parse(p?.date)
                     adapter!!.add(
