100b7694330233deaa34afdf0560d0ff7383b18d
Francislainy Campos <francislainy.campos@hmhco.com>
2020-07-25 17:57:42 +0100

    Hide settings cog for inner items and add intent to share app through Whatsapp. Delete ShareFragment.

--- app/src/main/java/com/example/covidtracker/activities/MainActivity.kt
+++ app/src/main/java/com/example/covidtracker/activities/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.covidtracker.activities
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.covidtracker.R
+import com.example.covidtracker.utils.gone
+import com.example.covidtracker.utils.visible
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.snippet_toolbar_plain.*
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -29,9 +34,41 @@ class MainActivity : AppCompatActivity() {
         NavigationUI.setupWithNavController(bottomNav, navController)
         bottomNav.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
 
-
         val appBarConfiguration = AppBarConfiguration(bottomNav.menu)
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+
+            when (destination.id) {
+
+                R.id.updatesFragment, R.id.contactTracingBottomFragment, R.id.checkInBottomFragment -> {
+                    ivSettingsCog.visible()
+                }
+
+                else -> {
+                    ivSettingsCog.gone()
+                }
+            }
+
+        }
+
+
+        bottomNav.setOnNavigationItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.share -> {
+                    shareViaWhatsApp()
+                    false
+                }
+                else -> {
+                    true
+                }
+            }
+
+        }
+
+
 
         ivSettingsCog.setOnClickListener {
 
@@ -45,9 +82,35 @@ class MainActivity : AppCompatActivity() {
                 R.id.covidCheckInFragment -> {
                     navController.navigate(R.id.action_covidCheckInFragment_to_settingsFragment)
                 }
+                R.id.share -> {
+                    shareViaWhatsApp()
+                }
 
             }
 
+        }
+
+
+    }
+
+
+    private fun shareViaWhatsApp(): Unit {
+        val whatsappIntent = Intent(Intent.ACTION_SEND)
+        whatsappIntent.type = "text/plain"
+        whatsappIntent.setPackage("com.whatsapp")
+        whatsappIntent.putExtra(
+            Intent.EXTRA_TEXT,
+            "Application of social rating share with your friend"
+        )
+        try {
+            Objects.requireNonNull(this).startActivity(whatsappIntent)
+        } catch (ex: ActivityNotFoundException) {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("http://play.google.com/store/apps/details?id=com.whatsapp")
+                )
+            )
         }
 
     }
--- app/src/main/java/com/example/covidtracker/fragments/ShareAppBottomFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.example.covidtracker.fragments
-
-import androidx.fragment.app.Fragment
-import com.example.covidtracker.R
-
-class ShareAppBottomFragment : Fragment(R.layout.fragment_share_app_bottom) {
-
-
-}
